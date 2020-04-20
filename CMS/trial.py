360c6c6cbde1e9822d4a28e883e42fb3d4897b04
GonelaD <34701091+GonelaD@users.noreply.github.com>
2020-04-20 17:34:31 +0530

    Add files via upload

--- CMS/trial.py
+++ CMS/trial.py
@@ -32,6 +32,73 @@ class LodgeComplaint(FlaskForm):
 
 
 @app.route('/')
+def base1():
+    return render_template('base1.html')
+
+@app.route('/user')
+def user():
+    return render_template('user.html')
+    
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
+@app.route('/afteruserloggedin')
+def afteruserloggedin():
+    return render_template('afteruserloggedin.html')
+
+@app.route('/afteradminloggedin')
+def afteradminloggedin():
+    return render_template('afteradminloggedin.html')
+    
+@app.route('/adminsettings')
+def adminsettings():
+    return render_template('adminsettings.html')
+@app.route('/adminhistory')
+def adminhistory():
+    return render_template('adminhistory.html')
+
+@app.route('/superadmin')
+def superadmin():
+    return render_template('superadmin.html')
+
+@app.route('/aftersuperadminloggedin')
+def aftersuperadminloggedin():
+    return render_template('aftersuperadminloggedin.html')
+
+@app.route('/adminresolvecomplaint')
+def adminresolvecomplaint():
+    return render_template('adminresolvecomplaint.html')
+
+@app.route('/superadminaddadmin')
+def superadminaddadmin():
+    return render_template('superadminaddadmin.html')
+
+@app.route('/superadminremoveadmin')
+def superadminremoveadmin():
+    return render_template('superadminremoveadmin.html')
+
+@app.route('/superadminadddivision')
+def superadminadddivision():
+    return render_template('superadminadddivision.html')
+    
+@app.route('/superadminremovedivision')
+def superadminremovedivision():
+    return render_template('superadminremovedivision.html')
+    
+@app.route('/superadminadddepartment')
+def superadminadddepartment():
+    return render_template('superadminadddepartment.html')
+
+
+@app.route('/superadminremovedepartment')
+def superadminremovedepartment():
+    return render_template('superadminremovedepartment.html')
+
+@app.route('/superadminsettings')
+def superadminsettings():
+    return render_template('superadminsettings.html')
+
 def index():
 	return render_template('index.html')
 
